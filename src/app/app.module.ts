20946bb291dbb3176ea3daeaf7cd83c47b6f667c
Ilian Vasilev <ivasilev@gmail.com>
2020-11-21 22:36:15 +0200

    initial SPA structure and bootstrap css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { ArticleCardComponent } from './common/article-card/article-card.component';
+import { UsersComponent } from './pages/users/users.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { PublicationsComponent } from './pages/publications/publications.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,    
+    ArticleCardComponent,
+    UsersComponent,
+    LoginComponent,
+    RegisterComponent,
+    PublicationsComponent
   ],
   imports: [
     BrowserModule,
